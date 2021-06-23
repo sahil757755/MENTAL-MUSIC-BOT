489c4b681c5e98b77443bafe6cf022b09dee6b46
TEX-X <83172693+sahil757755@users.noreply.github.com>
2021-06-23 12:04:04 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,17 @@
+from pyrogram import Client as Bot
+
+from tgcalls import run
+from config import API_ID, API_HASH, BOT_TOKEN
+
+
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="handlers")
+)
+
+bot.start()
+run()
+© 2021 GitHub, Inc.
